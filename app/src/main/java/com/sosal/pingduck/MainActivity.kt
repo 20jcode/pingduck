50f39d2926104470496652c875b5cef47c94af64
thgus0 <126738346+thgus0@users.noreply.github.com>
2024-05-25 01:47:35 +0900

    MainActivity와 MsgViewActivity 추가

---
+++ app/src/main/java/com/sosal/pingduck/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.sosal.pingduck
+
+import android.app.Activity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.EditText
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
+
+class MainActivity : AppCompatActivity(){
+    lateinit var CreateMsgBtn: Button;
+    lateinit var ViewMsgBtn: Button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CreateMsgBtn = findViewById<Button>(R.id.CreateBtn)
+        ViewMsgBtn = findViewById<Button>(R.id.ViewBtn)
+
+        ViewMsgBtn.setOnClickListener {
+            var intent = Intent(this, MsgViewActivity::class.java)
+            startActivityForResult(intent,100)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                // Do nothing, just return to MainActivity
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/sosal/pingduck/MsgViewActivity.kt
@@ -0,0 +1,20 @@
+package com.sosal.pingduck
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MsgViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_msg_view)
+
+        val cancelButton = findViewById<Button>(R.id.Cancel)
+        cancelButton.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+    }
+}
