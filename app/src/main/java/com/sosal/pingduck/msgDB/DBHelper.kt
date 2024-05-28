1231c83171db6ea08f14ab31f074118d30efc85f
mac <20j.code@gmail.com>
2024-05-28 17:57:26 +0900

    save

--- app/src/main/java/com/sosal/pingduck/msgDB/DBHelper.kt
+++ app/src/main/java/com/sosal/pingduck/msgDB/DBHelper.kt
@@ -3,6 +3,7 @@ package com.sosal.pingduck.msgDB
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import java.time.LocalDateTime
 
 class DBHelper(context: Context?, name: String?,
                factory : SQLiteDatabase.CursorFactory?,
@@ -12,7 +13,10 @@ class DBHelper(context: Context?, name: String?,
     override fun onCreate(db: SQLiteDatabase) {
         val sql : String = "CREATE TABLE if not exists msgtable (" +
                 "_id integer primary key autoincrement, " +
-                "txt text);";
+                "msgtarget text, " +
+                "msgpinktime text," +
+                "msgcreatetime datetime," +
+                "generatedmsg text);";
 
         db.execSQL(sql)
     }
@@ -23,4 +27,30 @@ class DBHelper(context: Context?, name: String?,
         db.execSQL(sql)
         onCreate(db)
     }
+
+    fun createMsg(msg : MsgDTO){
+        val msgtarget : String = msg.getMsgTarget()
+        val msgpinktime : String = msg.getMsgPinkTime()
+        val msgcreatetime : LocalDateTime = msg.getMsgCreateTime()
+
+        val sql : String = "insert into msgtable (" +
+                "msgtarget,msgpinktime,msgcreatetime,generatedmsg)" +
+                ""
+    }
+    /*
+    fun getMsgById(id:Int) : MsgDTO {
+
+    }
+
+
+     */
+
+    /**
+     * 생성된 핑계 메세지 정보를 조회
+     * @return MsgDTO
+     */
+    //fun getMsgList() : MutableList<MsgDTO> {
+
+    //}
+
 }
