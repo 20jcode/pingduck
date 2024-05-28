6288022c29a26c49f78c5da2cd117901b40b258f
mac <20j.code@gmail.com>
2024-05-28 17:42:48 +0900

    chore(MsgViewActivity) : 라인 변경

--- app/src/main/java/com/sosal/pingduck/MsgViewActivity.kt
+++ app/src/main/java/com/sosal/pingduck/MsgViewActivity.kt
@@ -6,11 +6,12 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MsgViewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_msg_view)
-
         val cancelButton = findViewById<Button>(R.id.Cancel)
         cancelButton.setOnClickListener {
             setResult(Activity.RESULT_CANCELED)
