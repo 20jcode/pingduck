79fffdb74adbdf73908655bc181e66955ee3a35d
mac <20j.code@gmail.com>
2024-05-28 20:57:38 +0900

    feat(DBHelper) : createMsg, getMsgById 추가

--- app/src/main/java/com/sosal/pingduck/msgDB/DBHelper.kt
+++ app/src/main/java/com/sosal/pingduck/msgDB/DBHelper.kt
@@ -1,6 +1,8 @@
 package com.sosal.pingduck.msgDB
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import java.time.LocalDateTime
@@ -16,7 +18,7 @@ class DBHelper(context: Context?, name: String?,
                 "msgtarget text, " +
                 "msgpinktime text," +
                 "msgcreatetime datetime," +
-                "generatedmsg text);";
+                "generatedmsg text);"
 
         db.execSQL(sql)
     }
@@ -28,22 +30,39 @@ class DBHelper(context: Context?, name: String?,
         onCreate(db)
     }
 
-    fun createMsg(msg : MsgDTO){
-        val msgtarget : String = msg.getMsgTarget()
-        val msgpinktime : String = msg.getMsgPinkTime()
-        val msgcreatetime : LocalDateTime = msg.getMsgCreateTime()
+    fun createMsg(db:SQLiteDatabase,msg : MsgDTO){
+        if(msg.isGenerated()){
+            val msgtarget : String = msg.getMsgTarget()
+            val msgpinktime : String = msg.getMsgPinkTime()
+            val msgcreatetime : String = msg.getMsgCreateTime()
+            val msgcreatedatas : String = msg.getGeneratedMsg()
 
-        val sql : String = "insert into msgtable (" +
-                "msgtarget,msgpinktime,msgcreatetime,generatedmsg)" +
-                ""
+            val sql : String = "INSERT into msgtable (" +
+                    "msgtarget,msgpinktime,msgcreatetime,generatedmsg)" +
+                    "VALUES" +
+                    "(msgtarget,msgpinktime,msgcreatetime,msgcreatedatas);"
+
+            db.execSQL(sql)
+        }
+
     }
-    /*
-    fun getMsgById(id:Int) : MsgDTO {
+
+    fun getMsgById(db:SQLiteDatabase,id:Int) : MsgDTO {
+        val db = this.readableDatabase
+        val selection = "_id = ?"
+        val selectionArgs = arrayOf(id.toString())
+        val cursor : Cursor = db.query("msgtable",null,selection,selectionArgs,null,null,null)
+        val msgAns : MsgDTO = MsgDTO(cursor.getString(cursor.getColumnIndexOrThrow("msgtarget")),
+            cursor.getString(cursor.getColumnIndexOrThrow("msgpinktime")),
+            cursor.getString(cursor.getColumnIndexOrThrow("msgcreatetime")),
+            )
+
+        msgAns.generateMsg(cursor.getString(cursor.getColumnIndexOrThrow("generatedmsg")))
+
+        return msgAns
 
     }
 
-
-     */
 
     /**
      * 생성된 핑계 메세지 정보를 조회
