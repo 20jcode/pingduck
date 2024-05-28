42aef6e987f39196c8182a7430b0652fa51407c7
mac <20j.code@gmail.com>
2024-05-28 22:02:53 +0900

    feat(CreateNewMsgActivity.kt) : 버튼 추가

--- app/src/main/java/com/sosal/pingduck/CreateNewMsgActivity.kt
+++ app/src/main/java/com/sosal/pingduck/CreateNewMsgActivity.kt
@@ -9,14 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.sosal.pingduck.msgDB.DBHelper
+import com.sosal.pingduck.msgDB.MsgDTO
 
 class CreateNewMsgActivity : AppCompatActivity() {
     //생성버튼
-    lateinit var createBtn : Button;
+    lateinit var createBtn : Button
     //초기화
-    lateinit var refrashBtn : Button;
+    lateinit var refrashBtn : Button
     //취소
-    lateinit var cancelBtn : Button;
+    lateinit var cancelBtn : Button
+
+    lateinit var msgTarget: ChipGroup
+
+    lateinit var msgPinkTime : ChipGroup
+
+    lateinit var msgPinkWhy : ChipGroup
 
     //DB 객체 선언
     lateinit var database : SQLiteDatabase
@@ -37,13 +44,15 @@ class CreateNewMsgActivity : AppCompatActivity() {
 
         //ChipGroup setting
         //동적으로 선택지를 추가해준다.
-        val msgTarget = findViewById<ChipGroup>(R.id.newMsgViewFocusTargetChipGroup)
-        val msgPinkTime = findViewById<ChipGroup>(R.id.newMsgViewTimeTextChipGroup)
-        val msgPinkWhy = findViewById<ChipGroup>(R.id.newMsgViewPinkWhyChipGroup)
+        msgTarget = findViewById<ChipGroup>(R.id.newMsgViewFocusTargetChipGroup)
+        msgPinkTime = findViewById<ChipGroup>(R.id.newMsgViewTimeTextChipGroup)
+        msgPinkWhy = findViewById<ChipGroup>(R.id.newMsgViewPinkWhyChipGroup)
 
+        //setChipBackgroundColorResource(R.color.teal_200)
         msgTarget.addView(Chip(this).apply{
             text = "교수님"
             isCloseIconVisible = true
+
         })
         msgTarget.addView(Chip(this).apply{
             text = "선배"
@@ -54,12 +63,27 @@ class CreateNewMsgActivity : AppCompatActivity() {
             isCloseIconVisible = true
         })
         msgPinkTime.addView(Chip(this).apply{
-            text = "test2"
+            text = "오늘"
+            isCloseIconVisible = true
+
+        })
+        msgPinkTime.addView(Chip(this).apply{
+            text = "다음생에 만남"
             isCloseIconVisible = true
 
         })
         msgPinkWhy.addView(Chip(this).apply{
-            text = "test3"
+            text = "그냥"
+            isCloseIconVisible = true
+
+        })
+        msgPinkWhy.addView(Chip(this).apply{
+            text = "금붕어 산책"
+            isCloseIconVisible = true
+
+        })
+        msgPinkWhy.addView(Chip(this).apply{
+            text = "곤충 채집"
             isCloseIconVisible = true
 
         })
