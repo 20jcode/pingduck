94af8d69b0e7d4a7bb0fcaa6123c8e51c7cfdfcb
thgus0 <126738346+thgus0@users.noreply.github.com>
2024-05-29 15:03:34 +0900

    save

--- app/src/main/java/com/sosal/pingduck/MsgViewActivity.kt
+++ app/src/main/java/com/sosal/pingduck/MsgViewActivity.kt
@@ -3,18 +3,32 @@ package com.sosal.pingduck
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-
+import com.sosal.pingduck.databinding.ActivityMainBinding
 
 class MsgViewActivity : AppCompatActivity() {
-
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_msg_view)
 
-            setResult(Activity.RESULT_CANCELED)
-            finish()
+        setResult(RESULT_CANCELED)
+        finish()
+
+        toggle = ActionBarDrawerToggle(this,binding.drawer, "OPEN", "CLOSE")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
         }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
